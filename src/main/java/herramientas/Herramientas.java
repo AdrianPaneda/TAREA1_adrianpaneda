fa7040675b1155788a396a2046e9b45902c815f0
adrip <adrip@DESKTOP-DLCLM4R>
2025-10-23 14:15:23 +0100

    Controlado en los regex que no se pueda introducir el caracter "|" para no romper el fichero credenciales.txt

--- src/main/java/herramientas/Herramientas.java
+++ src/main/java/herramientas/Herramientas.java
@@ -15,86 +15,96 @@ import java.util.regex.Pattern;
  * @author Adrian Pañeda Hamadi
  */
 public class Herramientas {
-    
-    /**
-     * Metodo para verificar email 
-     * @param email
-     * @return boolean 
-     */
-    public static boolean verificarEmail(String email){
-    
-        String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches(); 
-    }
-    
-    /**
-     * Metodo para verificar que se introduce un nombre valido
-     * @param nombre
-     * @return boolean
-     */
-    public static boolean verificarNombre(String nombre){
-        
-        String regex = "^[a-zA-áéíóúÁÉÍÓÚñÑ]+$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(nombre);
-        return matcher.matches();
-    }
-    
-    /**
-     * Metodo para verificar nombre de usuario
-     * @param usuario
-     * @return boolean
-     */
-    public static boolean verificarNombreusuario(String usuario){
-    
-    	String regex = "^[A-Za-z]{3,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(usuario);
-        return matcher.matches();
-    }
-    /**
-     * Metodo para verificar formato de contraseña
-     * @param contrasena
-     * @return boolean
-     */
-    public static boolean verificarContrasena(String contrasena) {
-    	
-    	String regex = "^[^\\s]{3,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(contrasena);
-        return matcher.matches();
-    }
-    /**
-     * Metodo para controlar las confirmaciones del menú
-     * @param confirmacion
-     * @return boolean
-     */
-    public static boolean confirmarRechazar(String confirmacion) {
-    	
-    	confirmacion = confirmacion.toLowerCase();
-    	if(confirmacion.equals("s")||confirmacion.equals("n")) {
-    		return true;
-    	}
-    	else return false;
-    }
-    /**
-     * Metodo para verificar fecha
-     * @param fecha
-     * @return boolean
-     */
-    public static boolean verificarFecha(String fecha) {
-    	
-    	 DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-         try {
-             LocalDate.parse(fecha, formato);
-             return true; 
-         } catch (DateTimeParseException e) {
-        	 System.out.println("\t Fecha no válida, intentelo de nuevo");
-             return false;
-         }	
- 
-    }
-  
+
+	/**
+	 * Metodo para verificar email
+	 * 
+	 * @param email
+	 * @return boolean
+	 */
+	public static boolean verificarEmail(String email) {
+
+		String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+
+	/**
+	 * Metodo para verificar que se introduce un nombre valido
+	 * 
+	 * @param nombre
+	 * @return boolean
+	 */
+	public static boolean verificarNombre(String nombre) {
+
+		String regex = "^[a-zA-áéíóúÁÉÍÓÚñÑ]+$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(nombre);
+		return matcher.matches();
+	}
+
+	/**
+	 * Metodo para verificar nombre de usuario
+	 * 
+	 * @param usuario
+	 * @return boolean
+	 */
+	public static boolean verificarNombreusuario(String usuario) {
+
+		String regex = "^[A-Za-z]{3,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(usuario);
+		return matcher.matches();
+	}
+
+	/**
+	 * Metodo para verificar formato de contraseña
+	 * 
+	 * @param contrasena
+	 * @return boolean
+	 */
+	public static boolean verificarContrasena(String contrasena) {
+
+		String regex = "^(?!.*\\|)[^\\s]{3,}$";
+
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(contrasena);
+		return matcher.matches();
+	}
+
+	/**
+	 * Metodo para controlar las confirmaciones del menú
+	 * 
+	 * @param confirmacion
+	 * @return boolean
+	 */
+	public static boolean confirmarRechazar(String confirmacion) {
+
+		confirmacion = confirmacion.toLowerCase();
+		if (confirmacion.equals("s") || confirmacion.equals("n")) {
+			return true;
+		} else
+			return false;
+	}
+
+	/**
+	 * Metodo para verificar fecha
+	 * 
+	 * @param fecha
+	 * @return boolean
+	 */
+	public static boolean verificarFecha(String fecha) {
+
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		try {
+			LocalDate.parse(fecha, formato);
+			return true;
+		} catch (DateTimeParseException e) {
+			System.out.println("\t Fecha no válida, intentelo de nuevo");
+			return false;
+		}
+
+	}
+
 }
